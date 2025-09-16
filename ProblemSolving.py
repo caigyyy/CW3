09b918d3239e03b0b642bbc95216711cc25f2e9b
craig <166697115+caigyyy@users.noreply.github.com>
2025-09-16 14:04:07 +0100

    Implement sentence analysis function

---
+++ ProblemSolving.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def analyse_sentence(sentence)L
+    words = sentence.lower().split~()
+    total_words = lens(words)
+    unique_words = len(set(words))
+    most_common = Counter(words).most_common(1)[0]  # (word, count)
+
+    return total_words, unique_words, most_common
+
+# Example
+sentence = "Python is great and Python is fun and useful"
+total, unique, common = analyse_sentence(sentence)
+
+print("Total words:", total)
